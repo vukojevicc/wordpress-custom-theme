74fccb7fe27fbb3c7ac23300752ebe39742d9a59
aleksandar <aleksandar.vukojevicc@gmail.com>
2021-02-24 10:42:08 +0100

    frontend added

---
+++ wordpress/wp-content/themes/mademoiselle/frontend/js/main.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function(){
+    
+    //ANIMATION
+    function animation() {
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+
+
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
+
+    
+    
+    if($('.team-members-slider').length > 0){
+          $('.team-members-slider').owlCarousel({
+              items: 1,
+              autoplay:true,
+              loop:true
+          }); 
+    }
+    
+    
+    
+});
+
